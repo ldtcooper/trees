caddfe27ee6b1e05dae42d100307771b3e7a3890
Logan Cooper <ldtcoop@gmail.com>
2017-11-16 10:59:52 -0800

    Add Node to BST file

--- js/bst.js
+++ js/bst.js
@@ -1,4 +1,40 @@
-import Node from '/nodes.js';
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.rightChild = null;
+    this.leftChild = null;
+  }
+
+  setParent(parent) {
+    this.parent = parent;
+  }
+
+  setLeftChild(left) {
+    this.leftChild = left;
+    left.setParent(this);
+  }
+
+  setRightChild(right) {
+    this.rightChild = right;
+    right.setParent(this);
+  }
+
+  endNode() {
+    if (this.rightChild || this.leftChild) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  toString() {
+    const thisVal = `Value: ${this.value}`;
+    const thisParent = `Parent Node: ${this.parent ? this.parent.value : "None"}`;
+    const thisRight = `Right Child: ${this.rightChild ? this.rightChild.value : "None"}`;
+    const thisLeft = `Left Child: ${this.leftChild ? this.leftChild.value : "None"}`;
+    return(`-----\n${thisVal}\n${thisParent}\n${thisRight}\n${thisLeft}\n-----`);
+  }
+}
 
 class BST {
   constructor(root) {
